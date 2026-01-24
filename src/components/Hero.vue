628a4eaa885ed3e2b709cee225df9fa4424bb119
1999fronda <episcope14@gmail.com>
2026-01-24 14:03:47 +0800

    Updatead hero ui and socials.json data

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,23 +1,27 @@
 <script setup lang="ts">
 import { useUserStore } from '../stores/user';
+import socials from '../data/socials.json';
 
 const userStore = useUserStore()
 </script>
 
 <template>
-    <UPageHero :title="`Hi, I am ${userStore.user.firstName}`" :ui="{ container: 'max-w-4xl' }">
+    <UPageHero :title="`Hi, I am ${userStore.user.firstName}`" :ui="{ container: 'max-w-4xl', footer: 'text-center' }">
         <template #description>
             A <span class="text-primary">{{ userStore.user.title }}</span> with a strong focus on Laravel, building
             clean,scalable, and user-focused web
             applications.
-        </template>
 
-        <template #links>
-            <div class="flex gap-2">
+            <div class="flex gap-2 justify-center mt-10">
                 <UButton :to="'/resume.pdf'" download target="_blank" label="Resume" icon="i-lucide-arrow-down"
                     size="xl" color="neutral" variant="subtle" class="cursor-pointer" />
                 <UButton label="Let's connect" size="xl" color="primary" variant="solid" class="cursor-pointer" />
             </div>
+        </template>
+
+        <template #footer>
+            <UButton v-for="social, i in socials" :key="i" :to="social.href" :target="social.target"
+                :icon="`simple-icons:${social.icon}`" :aria-label="social.name" color="neutral" variant="ghost" />
         </template>
     </UPageHero>
 </template>
