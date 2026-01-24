604cd86383f73b7f0d462fad4827a1a444f83990
1999fronda <episcope14@gmail.com>
2026-01-24 16:10:06 +0800

    Fixed redirect error on error 404 page

--- src/pages/notFound.vue
+++ src/pages/notFound.vue
@@ -1,23 +1,11 @@
-<script setup lang="ts">
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const handleRedirect = () => {
-  router.push('/')
-}
-</script>
-
 <template>
-  <UError
-    :clear="{
-      class: 'cursor-pointer'
-    }"
-    :error="{
-      statusCode: 404,
-      statusMessage: 'Page not found',
-      message: 'The page you are looking for does not exist.'
-    }"
-    @click="handleRedirect"
-  />
+  <UError :error="{
+    statusCode: 404,
+    statusMessage: 'Page not found',
+    message: 'The page you are looking for does not exist.'
+  }">
+    <template #links>
+      <UButton label="Back to home" to="/" />
+    </template>
+  </UError>
 </template>
