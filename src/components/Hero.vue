44d6cf7d3df9a9ca5dc5c058162c95bb6ff10375
1999fronda <episcope14@gmail.com>
2026-01-25 21:25:26 +0800

    Added micro-animations to the Hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,28 +1,77 @@
 <script setup lang="ts">
 import { useUserStore } from '../stores/user';
 import socials from '../data/socials.json';
+import { Motion } from 'motion-v';
 
 const userStore = useUserStore()
 </script>
 
 <template>
-    <UPageHero :title="`Hi, I am ${userStore.user.firstName}`" :ui="{ container: 'max-w-4xl', footer: 'text-center' }">
+    <UPageHero :ui="{ container: 'max-w-4xl', footer: 'text-center' }">
+        <template #title>
+            <Motion :initial="{
+                opacity: 0,
+                y: 20
+            }" :animate="{
+                opacity: 1,
+                y: 0
+            }" :transition="{
+                duration: 0.5,
+                delay: 0.1
+            }">
+                Hi, I am {{ userStore.user.firstName }}
+            </Motion>
+        </template>
         <template #description>
-            A <span class="text-primary">{{ userStore.user.title }}</span> with a strong focus on Laravel, building
-            clean,scalable, and user-focused web
-            applications.
-
-            <div class="flex gap-2 justify-center mt-10">
-                <UButton :to="'/resume.pdf'" download target="_blank" label="Resume" icon="i-lucide-arrow-down"
-                    size="xl" color="neutral" variant="subtle" class="cursor-pointer" />
-                <UButton label="Let's connect" :to="'mailto:' + userStore.user.email" size="xl" color="primary"
-                    variant="solid" class="cursor-pointer" />
-            </div>
+            <Motion :initial="{
+                opacity: 0,
+                y: 20
+            }" :animate="{
+                opacity: 1,
+                y: 0
+            }" :transition="{
+                duration: 0.5,
+                delay: 0.2
+            }">
+                A <span class="text-primary">{{ userStore.user.title }}</span> with a strong focus on Laravel, building
+                clean, scalable, and user-focused web
+                applications.
+            </Motion>
+            <Motion :initial="{
+                opacity: 0,
+                y: 20
+            }" :animate="{
+                opacity: 1,
+                y: 0
+            }" :transition="{
+                duration: 0.5,
+                delay: 0.3
+            }">
+                <div class="flex gap-2 justify-center mt-10">
+                    <UButton :to="'/resume.pdf'" download target="_blank" label="Resume" icon="i-lucide-arrow-down"
+                        size="xl" color="neutral" variant="subtle" class="cursor-pointer" />
+                    <UButton label="Let's connect" :to="'mailto:' + userStore.user.email" size="xl" color="primary"
+                        variant="solid" class="cursor-pointer" />
+                </div>
+            </Motion>
         </template>
 
         <template #footer>
-            <UButton v-for="social, i in socials" :key="i" :to="social.href" :target="social.target"
-                :icon="`simple-icons:${social.icon}`" :aria-label="social.name" color="neutral" variant="ghost" />
+            <div class="gap-x-2 inline-flex">
+                <Motion v-for="social, i in socials" :key="i" :initial="{
+                    opacity: 0,
+                    y: 20
+                }" :animate="{
+                    opacity: 1,
+                    y: 0
+                }" :transition="{
+                    duration: 0.5,
+                    delay: 0.4 + i * 0.08
+                }">
+                    <UButton :to="social.href" :target="social.target" :icon="`simple-icons:${social.icon}`"
+                        :aria-label="social.name" color="neutral" variant="ghost" />
+                </Motion>
+            </div>
         </template>
     </UPageHero>
 </template>
