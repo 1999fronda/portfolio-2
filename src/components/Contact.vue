26b2bab76c11072114c89ea8b261474f6f3aea36
1999fronda <episcope14@gmail.com>
2026-01-25 20:33:14 +0800

    Removed spotlight attribute on UPageSection

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,7 +8,7 @@ const userStore = useUserStore()
     <UPageSection id="contact" :ui="{ container: 'max-w-5xl' }">
         <UPageCTA title="Let's connect"
             description="I’m always open to chat about web development, design ideas, or new opportunities. Let’s make something awesome together."
-            variant="subtle" spotlight>
+            variant="subtle">
             <template #links>
                 <UButton label="Email me" :to="'mailto:' + userStore.user.email" size="xl" color="primary"
                     variant="solid" class="cursor-pointer" />
