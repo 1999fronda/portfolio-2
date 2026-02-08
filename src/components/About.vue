46bbae9d4109f49e0ce61d889f260df917e948d7
1999fronda <episcope14@gmail.com>
2026-02-08 23:44:55 +0800

    Updated about me

--- src/components/About.vue
+++ src/components/About.vue
@@ -14,12 +14,14 @@ const userStore = useUserStore()
         <template #description>
             <Motion v-bind="getDelayedMotion(0.3)">
                 <p class="mb-5">
-                    I’m <span class="font-medium">{{ userStore.fullName }}</span>, a <span class="font-medium">{{
-                        userStore.user.title }}</span> with a
-                    strong focus on <span class="font-medium">Laravel</span>. I’ve built
+                    I’m <span class="font-medium text-primary">{{ userStore.fullName }}</span>, a <span
+                        class="font-medium text-primary">{{
+                            userStore.user.title }}</span> with a
+                    strong focus on <span class="font-medium text-primary">Laravel</span>. I’ve built
                     multiple university-wide
                     web
-                    applications and currently use with <span class="font-medium">Vue.js</span> to create modern,
+                    applications and currently use with <span class="font-medium text-primary">Vue.js</span> to create
+                    modern,
                     dynamic
                     frontends. I’m passionate about
                     learning new technologies and continuously improving my craft.
