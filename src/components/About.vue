00f9fe7360d88103696635c759f07d43c5ec61cd
1999fronda <episcope14@gmail.com>
2026-02-08 23:18:38 +0800

    Added fade in animation on the About section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { Motion } from 'motion-v';
 import { useUserStore } from '../stores/user';
+import { getDelayedMotion } from '../utils/motionConfig';
 
 const userStore = useUserStore()
 </script>
@@ -10,21 +12,24 @@ const userStore = useUserStore()
             About Me
         </template>
         <template #description>
-            <p class="mb-5">
-                I’m <span class="font-medium">{{ userStore.fullName }}</span>, a <span class="font-medium">{{
-                    userStore.user.title }}</span> with a
-                strong focus on <span class="font-medium">Laravel</span>. I’ve built
-                multiple university-wide
-                web
-                applications and currently use with <span class="font-medium">Vue.js</span> to create modern, dynamic
-                frontends. I’m passionate about
-                learning new technologies and continuously improving my craft.
-            </p>
-            <p>
-                Lately, I’ve been exploring Android development with Kotlin to broaden my expertise. Outside
-                programming, I
-                enjoy watching anime and playing online games, which help me unwind and stay creative.
-            </p>
+            <Motion v-bind="getDelayedMotion(0.3)">
+                <p class="mb-5">
+                    I’m <span class="font-medium">{{ userStore.fullName }}</span>, a <span class="font-medium">{{
+                        userStore.user.title }}</span> with a
+                    strong focus on <span class="font-medium">Laravel</span>. I’ve built
+                    multiple university-wide
+                    web
+                    applications and currently use with <span class="font-medium">Vue.js</span> to create modern,
+                    dynamic
+                    frontends. I’m passionate about
+                    learning new technologies and continuously improving my craft.
+                </p>
+                <p>
+                    Lately, I’ve been exploring Android development with Kotlin to broaden my expertise. Outside
+                    programming, I
+                    enjoy watching anime and playing online games, which help me unwind and stay creative.
+                </p>
+            </Motion>
         </template>
     </UPageSection>
 </template>
